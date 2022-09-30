318b899bb6f92cd5832a8f7d543b8b3d28701739
korealu <643949593@qq.com>
2022-09-30 16:40:56 +0800

    小程序配置文件学习 2022-09-30 16:40:51

--- pages/base-study/basic-study.js
+++ pages/base-study/basic-study.js
@@ -6,7 +6,45 @@ Page({
    */
   data: {
     message: '字符串展示',
-    movieList: ['案例一', '案例二', '案例三', '案例四']
+    movieList: ['案例一', '案例二', '案例三', '案例四'],
+    fieldList: [
+      { name: '序号', props: 'id'},
+      { name: '书名', props: 'name'},
+      { name: '日期', props: 'date'},
+      { name: '价格', props: 'price'},
+      { name: '数量', props: 'count'},
+      { name: '操作', props: 'handler'},
+    ],
+    dataList: [
+      {
+        id: 1,
+        name: '《海底两万里》',
+        price: 99,
+        count: 3,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 2,
+        name: '《狂人日记》',
+        price: 66,
+        count: 1,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 3,
+        name: '《石头记》',
+        price: 210,
+        count: 2,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 4,
+        name: '《骆驼祥子》',
+        price: 430,
+        count: 1,
+        date: new Date().toLocaleString()
+      }
+    ]
   },
 
   getData () {
