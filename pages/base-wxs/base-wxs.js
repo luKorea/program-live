badec5eeacbb43c431fcd502dad1a46ab453633e
korealu <643949593@qq.com>
2022-10-10 13:53:12 +0800

    wxs语法学习 2022-10-10 13:53:00

---
+++ pages/base-wxs/base-wxs.js
@@ -0,0 +1,136 @@
+// pages/base-wxs/base-wxs.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataList: [{
+        id: 1,
+        name: '《海底两万里》',
+        price: 99,
+        count: 3,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 2,
+        name: '《狂人日记》',
+        price: 66,
+        count: 1,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 3,
+        name: '《石头记》',
+        price: 210,
+        count: 2,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 4,
+        name: '《骆驼祥子》',
+        price: 430,
+        count: 1,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 5,
+        name: '《骆驼祥子》',
+        price: 430,
+        count: 1,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 6,
+        name: '《骆驼祥子》',
+        price: 430,
+        count: 1,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 7,
+        name: '《骆驼祥子》',
+        price: 430,
+        count: 1,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 8,
+        name: '《骆驼祥子》',
+        price: 430,
+        count: 1,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 9,
+        name: '《骆驼祥子》',
+        price: 430,
+        count: 1,
+        date: new Date().toLocaleString()
+      },
+      {
+        id: 10,
+        name: '《骆驼祥子》',
+        price: 40,
+        count: 10,
+        date: new Date().toLocaleString()
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
