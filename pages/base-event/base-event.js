945367fb1375249a72d5f3203daaf8efa0bf3558
korealu <643949593@qq.com>
2022-10-10 16:09:10 +0800

    Mark语法学习 2022-10-10 16:09:06

--- pages/base-event/base-event.js
+++ pages/base-event/base-event.js
@@ -1,9 +1,20 @@
 // pages/base-event/base-event.js
 Page({
+  data: {
+    markData: {
+      namer: '这是Mark数据',
+      age: 20,
+      height: 30,
+      code: 'fjsklsdjf'
+    }
+  },
   handleClick(e) {
     console.log(e, '事件')
   },
   eventObject(e) {
     console.log(e, '事件对象')
+  },
+  markEvent(e) {
+    console.log(e, 'markUse')
   }
 })
